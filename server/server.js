86382ba5b8a301a0ddeaa10fa1c66acacf0f3616
Unknown <magicdustin14@gmail.com>
2017-11-01 21:34:28 -0700

    added socket.io to project

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 
 const express = require('express');
 var app = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
