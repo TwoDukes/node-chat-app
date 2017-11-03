eb5fc6a502e53125907a6493e1af5488a6e89e41
Unknown <magicdustin14@gmail.com>
2017-11-03 01:34:22 -0700

    Server will now emit all messages sent via socket.io

--- public/js/index.js
+++ public/js/index.js
@@ -3,16 +3,12 @@ var socket = io();
 socket.on('connect' ,function() {
     console.log('connected to server');   
 
-    socket.emit('createMessage', {
-        from: "XxTESTERxX",
-        text: "Hey WowZaPowZa!"
-    });
 });
 
  socket.on('disconnect', function() {
     console.log('Disconnected from server');
 });
 
-socket.on('NewMessage', function(message) {
-    console.log('NewMessage', message);
+socket.on('newMessage', function(message) {
+    console.log('newMessage', message);
 });
